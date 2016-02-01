d70fae54e84d2fd1685e155db6a8353b327e21e4
Bajwa <bik.bajwa@hpe.com>
2016-02-01 16:32:48 -0700

    fixed gem in deploy

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -2,7 +2,7 @@ git "#{Chef::Config[:file_cache_path]}/RailsApp" do
   repository "https://github.com/bikbajwa/RailsApplication.git"
 end
 
-gem "rails"
+gem_package "rails"
 
 execute "Start the rails server" do
   command "#{Chef::Config[:file_cache_path]}/RailsApp/bin/rails server -b 0.0.0.0"
