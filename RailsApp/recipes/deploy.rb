077bc4278d41b7abaeeeb2dc49a78fe4594703ac
Bajwa <bik.bajwa@hpe.com>
2016-02-02 10:07:35 -0700

    update rubygems

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -2,6 +2,10 @@ git "#{Chef::Config[:file_cache_path]}/RailsApp" do
   repository "https://github.com/bikbajwa/RailsApplication.git"
 end
 
+execute "update RubyGems" do
+  command "gem update --system"
+end
+
 gem_package "rails"
 
 execute "Start the rails server" do
