54bb86556fa8626469c8ec1067f44af6678e8aa8
Bajwa <bik.bajwa@hpe.com>
2016-02-02 10:47:59 -0700

    maybe it's already ready for rails?

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -10,7 +10,7 @@ execute "update RubyGems" do
   EOF
 end
 
-gem_package "rails"
+#gem_package "rails"
 
 execute "Start the rails server" do
   command "#{Chef::Config[:file_cache_path]}/RailsApp/bin/rails server -b 0.0.0.0"
