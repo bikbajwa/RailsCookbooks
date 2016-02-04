ccea64c46a3f9abf8ba62c2066db0897f611e937
Bajwa <bik.bajwa@hpe.com>
2016-02-04 15:07:03 -0700

    going to try deploying with deploy rb

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -1,21 +1,27 @@
-directory "/opt/Rails" do
-  action :create
-end
+#directory "/opt/Rails" do
+#  action :create
+#end
 
-git "/opt/Rails/RailsApp" do
-  repository "https://github.com/bikbajwa/RailsApplication.git"
-end
+#git "/opt/Rails/RailsApp" do
+#  repository "https://github.com/bikbajwa/RailsApplication.git"
+#end
 
-execute "update RubyGems" do
-  command <<-EOF
-    gem install ruby-gems-update
-    update_rubygems
-    gem update --system
-  EOF
-end
+#execute "update RubyGems" do
+#  command <<-EOF
+#    gem install ruby-gems-update
+#    update_rubygems
+#    gem update --system
+#  EOF
+#end
 
 #gem_package "rails"
 
-execute "Start the rails server" do
-  command "/opt/Rails/RailsApp/bin/rails server -b 0.0.0.0"
+#execute "Start the rails server" do
+#  command "/opt/Rails/RailsApp/bin/rails server -b 0.0.0.0"
+#end
+
+deploy "Rails Application" do
+  repo "https://github.com/bikbajwa/RailsApplication.git"
+  deploy_to "/tmp/RailsApplication"
+  action :deploy
 end
