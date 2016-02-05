e29875d018c00dbe0d2d64fc8c0f92e9db46a99f
Bajwa <bik.bajwa@hpe.com>
2016-02-05 14:34:42 -0700

    updated... things

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -20,15 +20,18 @@
 #  command "/opt/Rails/RailsApp/bin/rails server -b 0.0.0.0"
 #end
 
-package ["gcc", "ruby-devel", "zlib-devel"] do
+package ["gcc", "ruby-devel", "zlib-devel", "rubygems", "sqlite-devel"] do
   action :install
 end
-
-gem_package "rails"
 
 deploy "Rails Application" do
   repo "https://github.com/bikbajwa/RailsApplication.git"
   deploy_to "/tmp/RailsApplication"
-  restart_command "rails s -b 0.0.0.0"
+  #restart_command "rails s -b 0.0.0.0"
   action :deploy
+end
+
+cookbook_file "/etc/profile.d/ruby.sh" do
+  source "ruby.sh"
+  action :create
 end
