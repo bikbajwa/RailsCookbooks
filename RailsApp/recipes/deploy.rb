1c98e5eec3e70d0e822816e0b3f1562abba48764
Bajwa <bik.bajwa@hpe.com>
2016-02-05 14:21:11 -0700

    get rails going

--- RailsApp/recipes/deploy.rb
+++ RailsApp/recipes/deploy.rb
@@ -20,8 +20,15 @@
 #  command "/opt/Rails/RailsApp/bin/rails server -b 0.0.0.0"
 #end
 
+package ["gcc", "ruby-devel", "zlib-devel"] do
+  action :install
+end
+
+gem_package "rails"
+
 deploy "Rails Application" do
   repo "https://github.com/bikbajwa/RailsApplication.git"
   deploy_to "/tmp/RailsApplication"
+  restart_command "rails s -b 0.0.0.0"
   action :deploy
 end
