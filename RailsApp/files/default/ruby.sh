dadf225ca42dd5de6c6e51410a40bcfe08e8f1a3
Bajwa <bik.bajwa@hpe.com>
2016-02-11 15:04:57 -0700

    swapped path

--- RailsApp/files/default/ruby.sh
+++ RailsApp/files/default/ruby.sh
@@ -1,3 +1,3 @@
-export PATH="${PATH}:/usr/local/bin:/usr/local/share/ruby/gems/2.0/bin/"
+export PATH="${PATH}:/usr/local/share/ruby/gems/2.0/bin/:/usr/local/bin"
 export GEM_PATH="/usr/local/share/ruby/gems/2.0"
 export GEM_HOME="/usr/local/share/ruby/gems/2.0"
